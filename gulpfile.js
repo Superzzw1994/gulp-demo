a6ca7b9c087173d90ac6bd1e8bf2183d58e767e7
邪恶天才 <shzx1994529@gmail.com>
2021-04-20 13:34:41 +0800

    初始化gulp-demo

---
+++ gulpfile.js
@@ -0,0 +1,65 @@
+const {dest, src, watch, series, parallel} = require('gulp')
+const minHtml = require('gulp-htmlmin')
+const sass = require('gulp-sass');
+const babel = require('gulp-babel');
+const sourcemaps = require('gulp-sourcemaps');
+const terser = require('gulp-terser');
+const postcss = require('gulp-postcss')
+const inject = require('gulp-inject')
+const browsersync = require('browser-sync')
+const del = require('del')
+const htmlTask = () => {
+  return src('./src/*.html')
+    .pipe(minHtml({
+      collapseWhitespace: true
+    }))
+    .pipe(dest('./dist'))
+}
+const scssTask = () => src('./src/**/*.scss', {
+  base: './src'
+})
+  .pipe(sass().on('error', sass.logError))
+  .pipe(postcss())
+  .pipe(dest('./dist'));
+const jsTask = () => {
+  return src('./src/**/*.js', {
+    base: './src'
+  })
+    .pipe(babel())
+    .pipe(sourcemaps.init())
+    .pipe(terser({
+      mangle: {
+        toplevel: true
+      }
+    }))
+    .pipe(sourcemaps.write('./'))
+    .pipe(dest('./dist'))
+}
+const injectHtml = () => src('./src/*.html')
+  .pipe(inject(src(['./dist/js/*.js', './dist/css/*.css']), {
+    relative: true
+  }))
+  .pipe(dest('./dist'))
+const bs = browsersync.create()
+const serve = () => {
+  watch('./src/*.html', series(jsTask, injectHtml))
+  watch('./src/js/*.js', series(jsTask, injectHtml))
+  watch('./src/css/*.scss', series(jsTask, scssTask))
+
+  bs.init({
+    port: 1994,
+    open: true,
+    files: "./dist/*",
+    server: {
+      baseDir: './dist'
+    }
+  })
+}
+
+const clean = () => del(['dist'])
+const buildTask = series(clean, parallel(htmlTask, jsTask, scssTask), injectHtml)
+const serveTask = series(buildTask, serve)
+module.exports = {
+  serveTask,
+  buildTask
+}
---
+++ src/js/index.js
@@ -0,0 +1,3 @@
+const name = '123456'
+
+console.log(name)
