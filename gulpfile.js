76f9869d2b4bf14f62607973a221cca3c51d8137
zhiwei.zheng <zhiwei.zheng@cloudwise.com>
2021-04-20 15:20:53 +0800

    修改打包后html引入文件的路径为绝对路径

--- gulpfile.js
+++ gulpfile.js
@@ -6,60 +6,62 @@ const sourcemaps = require('gulp-sourcemaps');
 const terser = require('gulp-terser');
 const postcss = require('gulp-postcss')
 const inject = require('gulp-inject')
-const browsersync = require('browser-sync')
+const browserSync = require('browser-sync')
 const del = require('del')
 const htmlTask = () => {
-  return src('./src/*.html')
-    .pipe(minHtml({
-      collapseWhitespace: true
-    }))
-    .pipe(dest('./dist'))
+    return src('./src/*.html', {
+        base: './src'
+    })
+        .pipe(minHtml({
+            collapseWhitespace: true
+        }))
+        .pipe(dest('./dist'))
 }
 const scssTask = () => src('./src/**/*.scss', {
-  base: './src'
-})
-  .pipe(sass().on('error', sass.logError))
-  .pipe(postcss())
-  .pipe(dest('./dist'));
-const jsTask = () => {
-  return src('./src/**/*.js', {
     base: './src'
-  })
-    .pipe(babel())
-    .pipe(sourcemaps.init())
-    .pipe(terser({
-      mangle: {
-        toplevel: true
-      }
-    }))
-    .pipe(sourcemaps.write('./'))
-    .pipe(dest('./dist'))
+})
+    .pipe(sass().on('error', sass.logError))
+    .pipe(postcss())
+    .pipe(dest('./dist'));
+const jsTask = () => {
+    return src('./src/**/*.js', {
+        base: './src'
+    })
+        .pipe(babel())
+        .pipe(sourcemaps.init())
+        .pipe(terser({
+            mangle: {
+                toplevel: true
+            }
+        }))
+        .pipe(sourcemaps.write('./'))
+        .pipe(dest('./dist'))
 }
-const injectHtml = () => src('./src/*.html')
-  .pipe(inject(src(['./dist/js/*.js', './dist/css/*.css']), {
-    relative: true
-  }))
-  .pipe(dest('./dist'))
-const bs = browsersync.create()
+const injectHtml = () => {
+    return src("./dist/*.html", {base: './src'})
+        .pipe(inject(src(["./dist/js/*.js", "./dist/css/*.css"]), {relative: true}))
+        .pipe(dest('./dist'))
+}
+const bs = browserSync.create()
 const serve = () => {
-  watch('./src/*.html', series(jsTask, injectHtml))
-  watch('./src/js/*.js', series(jsTask, injectHtml))
-  watch('./src/css/*.scss', series(jsTask, scssTask))
+    watch('./src/*.html', series(jsTask, injectHtml))
+    watch('./src/js/*.js', series(jsTask, injectHtml))
+    watch('./src/css/*.scss', series(jsTask, scssTask, injectHtml))
 
-  bs.init({
-    port: 1994,
-    open: true,
-    files: "./dist/*",
-    server: {
-      baseDir: './dist'
-    }
-  })
+    bs.init({
+        port: 1994,
+        open: true,
+        files: "./dist/*",
+        server: {
+            baseDir: './dist'
+        }
+    })
 }
 
 const clean = () => del(['dist'])
 const buildTask = series(clean, parallel(htmlTask, jsTask, scssTask), injectHtml)
 const serveTask = series(buildTask, serve)
 module.exports = {
-  serveTask,
-  buildTask
+    serveTask,
+    buildTask
 }
